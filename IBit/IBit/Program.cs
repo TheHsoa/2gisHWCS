c9a2095db7212c257c246b9e88a686b771d0cb4c
Рассказов Александр <alexrassk@outlook.com>
2016-06-14 23:12:29 +0600

    1st HW

---
+++ IBit/IBit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IBit
+{
+    class IBit
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((n | 1 << (i - 1)) == n ? 1 : 0);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sum/Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sum
+{
+    class Sum
+    {
+        static void Main(string[] args)
+        {
+            int sum = Convert.ToInt32(Console.ReadLine());
+            sum += Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
