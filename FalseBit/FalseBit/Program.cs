59a00f6edc335226859ee92a4c80db6ffdc99803
Рассказов Александр <alexrassk@outlook.com>
2016-06-15 20:41:02 +0600

    SecondTry

---
+++ FalseBit/FalseBit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FalseBit
+{
+    class FalseBit
+    {
+        static int MostSignificantBit(int n)
+        {
+            int l = 0;
+            while (n != 0)
+            {
+                n >>= 1;
+                l++;
+            }
+            return l;
+        }
+                static void Main(string[] args)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(m & ~(1 << (MostSignificantBit(m) - 1)));
+            Console.ReadLine();
+
+        }
+    }
+}
+
