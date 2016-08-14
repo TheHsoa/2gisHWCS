006ff3d2a7700617bc081b361cc4c498f87ea4c5
Рассказов Александр <alexrassk@outlook.com>
2016-08-14 18:33:55 +0700

    L2_Z2

---
+++ L2_Z2/L2_Z2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//Лекция 2, слайд 12
+//Заполнить с консоли массив из N элементов. Отсортировать. Вывести результат. N - задается с консоли
+namespace L2_Z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число элементов в массиве: ");
+            var n = Convert.ToInt32(Console.ReadLine());
+
+            var arr = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i}-й элемент массива:");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+
+            Console.WriteLine("Результирующий отсортированный массив:");
+            for (int i = 0; i < n; i++)
+            {                
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
